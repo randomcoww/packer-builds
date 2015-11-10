55413bb14033354a1caa49e6d82ac8185942c2d2
randomcoww <randomcoww@dev6.static.local>
2015-11-10 00:37:22 -0800

    Fix serial console issues

--- cookbooks/serial_console/providers/grub_append.rb
+++ cookbooks/serial_console/providers/grub_append.rb
@@ -2,7 +2,7 @@ require 'chef/mixin/shell_out'
 include Chef::Mixin::ShellOut
 
 def load_current_resource
-  @current_resource = Chef::Resource::GrubAppend.new(new_resource.name)
+  @current_resource = Chef::Resource::SerialConsoleGrubAppend.new(new_resource.name)
   @current_resource
 end
 
@@ -12,7 +12,7 @@ end
 
 def add_lines
   new_resource.add_lines.each { |e|
-    file_editor.insert_line_if_no_match("/^#{e}$/", e)
+    file_editor.insert_line_if_no_match("/#{e}/", e)
   }
   file_editor.write_file
 end
@@ -24,7 +24,7 @@ def modify_lines
 end
 
 def make_grub_conf
-  shell_out!(new_resource.mkconfg_command)
+  shell_out!(new_resource.mkconfig_command)
 end
 
 def action_run
