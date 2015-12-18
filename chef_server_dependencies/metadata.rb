9cf8e4bd921d7a18576d7265c885e4938a82b3af
randomcoww <randomcoww@dev01.dyn.local>
2015-12-18 11:09:48 +0000

    Added NFS cookbook dependency. Renamed chef server dependencies cookbook

---
+++ chef_server_dependencies/metadata.rb
@@ -0,0 +1,8 @@
+name             'chef_server_dependencies'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef_server_dependencies'
+long_description 'Installs/Configures chef_server_dependencies'
+version          '0.1.0'
+depends          'chef-client'
