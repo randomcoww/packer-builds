f8efe376485ec05e88d7c8c1081b83f1f614f3ba
randomcoww <randomcoww@dev01.dyn.local>
2015-12-18 08:18:44 +0000

    Combined packer KVM into one repo

--- cookbooks/docker_wrapper/resources/container_service.rb
+++
@@ -1,5 +0,0 @@
-actions :start, :enable, :disable, :stop
-default_action :start
-
-attribute :service, :kind_of => [String], :name_attribute => true
-attribute :container_name, :kind_of => [String]
--- cookbooks/ms_dotnet45/Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'berkshelf',  '~> 3.2.1'
-gem 'chefspec',   '~> 4.1.1'
-gem 'foodcritic', '~> 4.0.0'
-gem 'rubocop',    '~> 0.27.1'
