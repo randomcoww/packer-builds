cb5976e8263e1b8c33da04f2e16ab5f032961bc8
randomcoww <randomcoww@dev02.dyn.local>
2015-12-25 11:35:00 +0000

    Updated cookbooks and docker server build

--- cookbooks/docker_wrapper/recipes/service.rb
+++ cookbooks/docker_wrapper/recipes/service.rb
@@ -1,4 +1,12 @@
-docker_service 'default' do
-  icc false
-  action [:create]
-end
+#docker_service 'default' do
+#  icc false
+#  action [:create]
+#end
+
+node['docker_service'].each_pair { |k, v|
+  res = docker_service 'docker' do
+    action :nothing
+  end
+  res.send(k.to_sym, v) if res.respond_to?(k)
+  res.run_action :create
+}
