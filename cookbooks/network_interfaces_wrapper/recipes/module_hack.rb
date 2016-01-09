ff25a3ac105a22d6cd7a826ab3b4de6f098e1624
randomcoww <randomcoww@dev02.dyn.local>
2016-01-09 18:07:18 +0000

    Updated cookbooks

--- cookbooks/network_interfaces_wrapper/recipes/module_hack.rb
+++ cookbooks/network_interfaces_wrapper/recipes/module_hack.rb
@@ -22,11 +22,3 @@ service 'kmod' do
   ignore_failure true
   action :nothing
 end
-
-node['network_interfaces'].each { |dev, opt|
-  res = Chef::Resource::NetworkInterface::Debian.new(dev, run_context)
-  opt.each { |k, v|
-    res.send(k.to_sym, v) if res.respond_to?(k)
-  }
-  res.run_action :create
-}
