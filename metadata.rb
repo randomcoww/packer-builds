31c5918ac5e2ccb688926c657367919f5b2eb962
randomcoww <randomcoww@vm2.static.local>
2016-07-01 18:53:37 -0700

    Cleanup

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,4 @@ license          'All rights reserved'
 description      'Installs/Configures packer_kvm'
 long_description 'Installs/Configures packer_kvm'
 version          '0.2.1'
-depends          'docker_wrapper'
-depends          'resolver'
-depends	         'network_interfaces_wrapper'
-depends          'nfs_export_wrapper'
+vpn_config
