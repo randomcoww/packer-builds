0cf61b79514f4cdd93823c95c2aa1cad2de26332
randomcoww <etherblimp@gmail.com>
2016-11-25 13:01:41 -0800

    cleanup

--- Berksfile
+++
@@ -1,5 +0,0 @@
-source "https://supermarket.chef.io"
-
-metadata
-
-cookbook 'vpn_config', git: 'https://github.com/randomcoww/chef-vpn_config.git'
--- metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'packer_kvm'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures packer_kvm'
-long_description 'Installs/Configures packer_kvm'
-version          '0.2.1'
-vpn_config
