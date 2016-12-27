a1ad47d97dff9f9a93824042659f41ad4278e253
randomcoww <etherblimp@gmail.com>
2016-12-27 10:16:58 -0800

    add jessie zfs build

--- packer-debian/scripts/disk_spindown.sh
+++ packer-debian/scripts/disk_spindown.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "enable disk spin down after 1 hour"
 
+apt-get -y install hdparm
 cat > /etc/udev/rules.d/50-hdparm.rules <<EOF
 ACTION=="add|change", KERNEL=="sd*", RUN+="/sbin/hdparm -B 127 -S 242 /dev/%k"
 EOF
