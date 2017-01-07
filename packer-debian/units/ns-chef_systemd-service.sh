db192c4edea07ce3a1b12dc35742693e26a537ff
randomcoww <etherblimp@gmail.com>
2017-01-07 10:07:06 -0800

    move units to chef deploy

---
+++ packer-debian/units/ns-chef_systemd-service.sh
@@ -0,0 +1,18 @@
+cat > /etc/systemd/system/chef_systemd.service <<EOF
+[Unit]
+Description=chef_systemd
+After=docker.service
+BindsTo=docker.service
+Requires=etc-chef.mount
+
+[Service]
+TimeoutStartSec=0
+Restart=on-failure
+RestartSec=20
+ExecStartPre=/usr/bin/chef-client -c /etc/chef/client.rb -o 'role[systemd_units_ns]'
+ExecStart=/usr/bin/chef-client -c /etc/chef/client.rb -i 300 -s 30 -o 'role[systemd_units_ns]'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable chef_systemd.service
