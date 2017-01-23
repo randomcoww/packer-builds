d425469db66270b6195d0cb978122a613a3fcc7c
randomcoww <etherblimp@gmail.com>
2017-01-23 03:07:12 -0800

    move stuff around

--- packer-debian/scripts/disk_spindown.sh
+++ packer-debian/scripts/disk_spindown.sh
@@ -5,5 +5,5 @@ apt-get -y install hdparm
 
 ## jessie was not loading rules from /etc/udev/rules.d, put it in /lib/udev/rules.d
 cat > /lib/udev/rules.d/91-hdparm.rules <<EOF
-ACTION=="add|change", KERNEL=="sd*", RUN+="/sbin/hdparm -B 127 -S 242 /dev/%k"
+ACTION=="add|change", KERNEL=="sd*", RUN+="/sbin/hdparm -B 127 -S 243 /dev/%k"
 EOF
