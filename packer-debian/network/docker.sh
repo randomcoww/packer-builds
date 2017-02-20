ca53313d68763bba6cc44bdc27275554ce0291e4
randomcoww <etherblimp@gmail.com>
2017-02-19 17:32:31 -0800

    update docker build network

---
+++ packer-debian/network/docker.sh
@@ -0,0 +1,111 @@
+## LAN bridge
+
+cat > /etc/systemd/network/eth0.network <<EOF
+[Match]
+Name=eth0
+
+[Network]
+DHCP=no
+Bridge=brlan
+EOF
+
+cat > /etc/systemd/network/brlan.netdev <<EOF
+[NetDev]
+Kind=bridge
+Name=brlan
+EOF
+
+cat > /etc/systemd/network/brlan.network <<EOF
+[Match]
+Name=brlan
+
+[Network]
+DHCP=yes
+EOF
+
+## Storage LAN bridge
+
+cat > /etc/systemd/network/eth1.network <<EOF
+[Match]
+Name=eth1
+
+[Network]
+DHCP=no
+Bridge=brstore
+EOF
+
+cat > /etc/systemd/network/brstore.netdev <<EOF
+[NetDev]
+Kind=bridge
+Name=brstore
+EOF
+
+cat > /etc/systemd/network/brstore.network <<EOF
+[Match]
+Name=brstore
+
+[Network]
+LinkLocalAddressing=ipv4
+DHCP=no
+EOF
+
+## VPN bridge
+
+cat > /etc/systemd/network/eth2.network <<EOF
+[Match]
+Name=eth2
+
+[Network]
+DHCP=no
+Bridge=brvpn
+EOF
+
+cat > /etc/systemd/network/tap.network <<EOF
+[Match]
+Name=tap*
+
+[Network]
+DHCP=no
+Bridge=brvpn
+EOF
+
+cat > /etc/systemd/network/brvpn.netdev <<EOF
+[NetDev]
+Kind=bridge
+Name=brvpn
+EOF
+
+cat > /etc/systemd/network/brvpn.network <<EOF
+[Match]
+Name=brvpn
+
+[Network]
+LinkLocalAddressing=no
+DHCP=no
+EOF
+
+## WAN
+
+cat > /etc/systemd/network/eth3.network <<EOF
+[Match]
+Name=eth3
+
+[Network]
+DNS=192.168.63.250
+DNS=192.168.63.249
+DNS=8.8.8.8
+LinkLocalAddressing=no
+DHCP=yes
+
+[DHCP]
+UseDNS=false
+UseNTP=false
+SendHostname=false
+UseHostname=false
+UseDomains=false
+UseTimezone=no
+EOF
+
+## load rules (not sure if needed)
+
+systemctl daemon-reload
