4b85aa6a7105552c05c3e4272cf1e637f1710330
randomcoww <etherblimp@gmail.com>
2017-03-08 00:46:40 -0800

    naming consistency

--- packer-debian/scripts/install_chef.sh
+++ packer-debian/scripts/install_chef.sh
@@ -8,7 +8,7 @@ apt-get clean
 
 ## mount chef secret from host
 ## copy this method: https://github.com/coreos/coreos-cloudinit/blob/master/units/90-configdrive.rules
-cat > /lib/udev/rules.d/80-etc-chef.rules <<EOF
+cat > /lib/udev/rules.d/80-chef-secret-mount.rules <<EOF
 ACTION!="add|change", GOTO="chef_secret_end"
 
 SUBSYSTEM=="virtio", DRIVER=="9pnet_virtio", ATTR{mount_tag}=="chef-secret", TAG+="systemd", ENV{SYSTEMD_WANTS}+="etc-chef.mount"
--- packer-debian/scripts/install_cloud-init.sh
+++ packer-debian/scripts/install_cloud-init.sh
@@ -2,11 +2,12 @@
 echo "install cloud-init"
 
 apt-get update -y
-apt-get -y install cloud-init netstat
+apt-get -y install cloud-init net-tools
 apt-get clean
 mkdir -p /var/lib/cloud/seed/nocloud-net
 
-cat > /lib/udev/rules.d/10-cloud-init.rules <<EOF
+## copy this method: https://github.com/coreos/coreos-cloudinit/blob/master/units/90-configdrive.rules
+cat > /lib/udev/rules.d/10-cloud-init-mount.rules <<EOF
 ACTION!="add|change", GOTO="cloud_init_end"
 
 SUBSYSTEM=="virtio", DRIVER=="9pnet_virtio", ATTR{mount_tag}=="cloud-init", TAG+="systemd", ENV{SYSTEMD_WANTS}+="var-lib-cloud-seed-nocloud\x2dnet.mount"
