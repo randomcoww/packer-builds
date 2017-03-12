29bcfe7997b4a8ca35d66d671f9afa665d99c1e2
randomcoww <etherblimp@gmail.com>
2017-03-12 16:57:53 -0700

    fix chef-client path

--- packer-debian/units/chef-client.sh
+++ packer-debian/units/chef-client.sh
@@ -7,7 +7,7 @@ After=etc-chef.mount
 TimeoutStartSec=0
 Restart=on-failure
 RestartSec=20
-ExecStart=chef-client -c /etc/chef/client.rb -i 600 -s 60 -o 'role[node-\$HOSTNAME]'
+ExecStart=/usr/bin/chef-client -c /etc/chef/client.rb -i 600 -s 60 -o 'role[node-\$HOSTNAME]'
 
 [Install]
 WantedBy=multi-user.target
