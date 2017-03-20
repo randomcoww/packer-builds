3f6fcf82fa4146f1374e74c89b7dda58aed27733
randomcoww <etherblimp@gmail.com>
2017-03-19 20:30:35 -0700

    cleanup

--- debian/scripts/disk_spindown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "enable disk spin down after 1 hour"
-
-apt-get -y install hdparm
-
-## jessie was not loading rules from /etc/udev/rules.d, put it in /lib/udev/rules.d
-cat > /lib/udev/rules.d/91-hdparm.rules <<EOF
-ACTION=="add|change", KERNEL=="sd*", RUN+="/sbin/hdparm -B $DISK_POWERSAVE -S $DISK_SPINDOWN /dev/%k"
-EOF
--- debian/scripts/ssh_authorized_keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "write ssh authorized keys"
-
-mkdir -p ~/.ssh
-
-cat > ~/.ssh/authorized_keys <<EOF
-$SSH_AUTHORIZED_KEYS
-EOF
