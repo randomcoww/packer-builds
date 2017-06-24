ac2ad0b4ef21fc15de85e0da31babe0a5e536037
randomcoww <etherblimp@gmail.com>
2017-06-24 02:50:49 -0700

    cleaner cloud-init local

--- debian/scripts/cloud-init-local-module.sh
+++ debian/scripts/cloud-init-local-module.sh
@@ -1,8 +1,16 @@
-## this allows systemd-networkd to be configured through write-file by running calling cloud-init init
-## in cloud-init-local.service before network comes up
-## should be removed for regular use
-mkdir -p /etc/systemd/system/cloud-init-local.service.d
-cat > /etc/systemd/system/cloud-init-local.service.d/init_local_module.conf <<EOF
-[Service]
-ExecStart=/usr/bin/cloud-init init
+## allow cloud-init init (non local) to run from no-cloud before network is up
+## for environments where this is desirable
+mkdir -p /etc/systemd/system/cloud-init.service.d
+cat > /etc/systemd/system/cloud-init.service.d/dropin.conf <<EOF
+[Unit]
+Wants=
+Wants=network-pre.target
+After=
+After=cloud-init-local.service
+Before=
+Before=NetworkManager.service
+Before=network-pre.target
+Before=shutdown.target
+Before=sysinit.target
+RequiresMountsFor=/var/lib/cloud
 EOF
