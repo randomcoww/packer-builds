6af7aa3e52b843813af7fddd3f9a9ca65b499c0a
randomcoww <etherblimp@gmail.com>
2017-08-04 16:48:41 -0700

    zfs ram limit, add more kubernetes downloads

--- debian-guest/scripts/install_kubelet.sh
+++ debian-guest/scripts/install_kubelet.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 echo "install kubelet minimal config"
 
-wget -O /usr/local/bin/kubelet https://storage.googleapis.com/kubernetes-release/release/v$KUBERNETES_VERSION/bin/linux/amd64/kubelet
+## kubelet
+wget -O /usr/local/bin/kubelet https://storage.googleapis.com/kubernetes-release/release/v$KUBERNETES_VERSION/bin/linux/amd64/kubelet && \
 chmod +x /usr/local/bin/kubelet
 
-wget -O /usr/local/bin/kube-proxy https://storage.googleapis.com/kubernetes-release/release/v$KUBERNETES_VERSION/bin/linux/amd64/kube-proxy
+## kube-proxy
+wget -O /usr/local/bin/kube-proxy https://storage.googleapis.com/kubernetes-release/release/v$KUBERNETES_VERSION/bin/linux/amd64/kube-proxy && \
 chmod +x /usr/local/bin/kube-proxy
 
+## etcd
+wget -O /tmp/etcd.tar.gz https://github.com/coreos/etcd/releases/download/v$ETCD_VERSION/etcd-v$ETCD_VERSION-linux-amd64.tar.gz && \
+tar xzf /tmp/etcd.tar.gz --wildcards --strip-components=1 -C /usr/local/bin */etcdctl */etcd && \
+rm /tmp/etcd.tar.gz
 
+## flannel
+wget -O /tmp/flannel.tar.gz https://github.com/coreos/flannel/releases/download/v$FLANNEL_VERSION/flannel-v$FLANNEL_VERSION-linux-amd64.tar.gz && \
+tar xzf /tmp/flannel.tar.gz -C /usr/local/bin 'flanneld' && \
+rm /tmp/flannel.tar.gz
+
+## cni plugins
+wget -O /tmp/cni-plugins.tar.gz https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGIN_VERSION/cni-plugins-amd64-v$CNI_PLUGIN_VERSION.tgz && \
+mkdir -p /opt/cni/bin/ && tar xzf /tmp/cni-plugins.tar.gz -C /opt/cni/bin && \
+rm /tmp/cni-plugins.tar.gz
+
+
+## kubelet systemd
 mkdir -p /etc/kubernetes/manifests
 cat > /etc/systemd/system/kubelet.service <<EOF
 [Unit]
--- debian-kvm-host/scripts/install_zfs.sh
+++ debian-kvm-host/scripts/install_zfs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "install ZFS on Linux"
 
+echo options zfs zfs_arc_max=$ZFS_ARC_MAX >> /etc/modprobe.d/zfs.conf
+
 echo deb http://ftp.debian.org/debian $(lsb_release -cs) contrib >> /etc/apt/sources.list.d/contrib.list
 apt-get -y update
 apt-get -y install linux-headers-$(uname -r)
