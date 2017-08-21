e308d3fb9805adc44611434199ba69d6f17f83fb
debian <debian@desktop.lan>
2017-08-20 20:49:42 -0700

    remove old builds

--- debian-kvm-host/scripts/disk_spindown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "enable disk spin down"
-
-apt-get -y update
-apt-get -y install hdparm
-
-cat > /etc/udev/rules.d/91-hdparm.rules <<EOF
-ACTION=="add|change", KERNEL=="sd*", RUN+="/sbin/hdparm -B $DISK_POWERSAVE -S $DISK_SPINDOWN /dev/%k"
-EOF
--- debian-kvm-host/scripts/install_cloud-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "install cloud-init"
-
-apt-get update -y
-apt-get -y install cloud-init net-tools
-
-mkdir -p /var/lib/cloud/seed/nocloud-net
