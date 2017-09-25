604f80fc88716a6ff95287e73309405de67d1203
randomcoww <randomcoww@desktop.hl.lan>
2017-09-24 21:33:15 -0700

    unifi update

--- debian/scripts/finish.sh
+++ debian/scripts/finish.sh
@@ -10,6 +10,9 @@ update-grub
 apt-get -y autoremove
 apt-get clean
 
+## remove fallback network
+rm /etc/systemd/network/99-fallback.network
+
 ## if these are blank, they are generated on first boot
 echo -n > /var/lib/dbus/machine-id
 echo -n > /etc/machine-id
